6c7049aa7dc7284090a2488e4c4b11a7f0bd65a1
ShebangDog <e1n18088@oit.ac.jp>
2020-11-07 00:48:48 +0900

    遷移用の拡張関数を定義する

--- app/src/main/java/dog/shebang/voiceoflabor/base/screen/Screen.kt
+++ app/src/main/java/dog/shebang/voiceoflabor/base/screen/Screen.kt
@@ -1,6 +1,12 @@
 package dog.shebang.voiceoflabor.base.screen
 
+import androidx.navigation.NavController
+import androidx.navigation.compose.navigate
+
 sealed class Screen(val name: String) {
     object RecList : Screen("reclist")
     object Recording : Screen("recording")
 }
+
+fun NavController.navigateToRecList() = navigate(Screen.RecList.name)
+fun NavController.navigateToRecording() = navigate(Screen.Recording.name)
