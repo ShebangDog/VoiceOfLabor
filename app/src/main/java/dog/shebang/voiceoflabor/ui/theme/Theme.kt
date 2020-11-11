8415bf5fe5da6b6cc139974004f28f027de8f75d
ShebangDog <e1n18088@oit.ac.jp>
2020-11-12 02:42:11 +0900

    パッケージを移動させる

--- app/src/main/java/dog/shebang/voiceoflabor/ui/theme/Theme.kt
+++ app/src/main/java/dog/shebang/voiceoflabor/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package dog.shebang.voiceoflabor.theme
+package dog.shebang.voiceoflabor.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
