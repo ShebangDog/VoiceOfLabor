360ab21096ac1ad4f39710af532228eb61e0042d
ShebangDog <e1n18088@oit.ac.jp>
2020-11-11 15:19:08 +0900

    音声ファイルの保存先を記録できるようにする

--- app/src/main/java/dog/shebang/voiceoflabor/data/VoiceRepository.kt
+++ app/src/main/java/dog/shebang/voiceoflabor/data/VoiceRepository.kt
@@ -1,7 +1,13 @@
 package dog.shebang.voiceoflabor.data
 
+import dog.shebang.voiceoflabor.data.db.ApplicationDataBase
+import dog.shebang.voiceoflabor.data.db.InternalStorageDataSource
+import dog.shebang.voiceoflabor.data.db.VoiceEntity
 import dog.shebang.voiceoflabor.model.Voice
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 interface VoiceRepository {
     fun fetchVoice(fileName: String): Flow<Voice?>
@@ -9,4 +15,30 @@ interface VoiceRepository {
     fun fetchVoiceList(): Flow<List<Voice>>
 
     suspend fun saveVoice(fileName: String)
+}
+
+class DefaultVoiceRepository(
+    private val database: ApplicationDataBase,
+    private val internalStorageDataSource: InternalStorageDataSource
+) : VoiceRepository {
+
+    @FlowPreview
+    @ExperimentalCoroutinesApi
+    override fun fetchVoice(fileName: String): Flow<Voice?> {
+        val uri = internalStorageDataSource.formatToAccess(fileName)
+
+        return database.voiceDao().fetch(uri).map { Voice(uri) }
+    }
+
+    @FlowPreview
+    @ExperimentalCoroutinesApi
+    override fun fetchVoiceList(): Flow<List<Voice>> = database.voiceDao().fetchAll()
+        .map { list -> list.map { Voice(it.uri) } }
+
+    override suspend fun saveVoice(fileName: String) {
+        val uri = internalStorageDataSource.formatToAccess(fileName)
+
+        database.voiceDao().insert(VoiceEntity(uri))
+    }
+
 }
